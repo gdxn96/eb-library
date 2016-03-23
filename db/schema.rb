705347971882550190e5d3fbf3d4fabfec802f7c
George Dixon <georgedixon@Sarahs-MacBook-Pro-5.local>
2016-03-23 16:29:53 +0000

    Shows users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323115933) do
+ActiveRecord::Schema.define(version: 20160323161119) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160323115933) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end
