f413616f6bb604284498db4cf5842e804d975f4b
George Dixon <georgedixon@Sarahs-MacBook-Pro-5.local>
2016-03-24 12:11:49 +0000

    sdfghjnbvgyuj

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get 'help', to: "static_pages#help"
   get 'contact', to: "static_pages#contact"
@@ -12,6 +16,8 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
