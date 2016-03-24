c428915054158ef730e1121f206d24a4933fe06f
George Dixon <georgedixon@Sarahs-MacBook-Pro-5.local>
2016-03-24 11:53:35 +0000

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323161119) do
+ActiveRecord::Schema.define(version: 20160324110722) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -24,11 +24,14 @@ ActiveRecord::Schema.define(version: 20160323161119) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
